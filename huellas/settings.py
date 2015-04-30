653e5deefc905be05d6261ad401197962846b9f1
Gustavo Velasco <gustavo.velascoh@gmail.com>
2015-04-30 11:00:32 -0500

    db change

--- huellas/settings.py
+++ huellas/settings.py
@@ -76,6 +76,8 @@ WSGI_APPLICATION = 'huellas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,6 +85,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
