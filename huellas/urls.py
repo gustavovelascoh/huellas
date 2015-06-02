c7111300ffa27a9e1472dcd91bbbfae16e145a6c
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2015-06-02 14:33:24 -0500

    Changes to include django-allauth for facebook authentication

--- huellas/urls.py
+++ huellas/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'huellas.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^admin/', include(admin.site.urls)),    
     url(r'', include('pets.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
